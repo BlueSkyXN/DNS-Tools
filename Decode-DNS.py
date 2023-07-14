33f360f801b2d672ba79dc4c483a3f59803e4d19
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-07-14 15:06:27 +0800

    0.0.2

--- Decode-DNS.py
+++ Decode-DNS.py
@@ -1,20 +1,40 @@
+import argparse
 import base64
 from dns import message
 
-def decode_and_parse_dns_packet(base64_data):
+def get_dns_packet_id(base64_data):
+    decoded_data = base64.b64decode(base64_data)
+    dns_packet = message.from_wire(decoded_data)
+    return dns_packet.id
+
+def get_dns_packet_questions(base64_data):
+    decoded_data = base64.b64decode(base64_data)
+    dns_packet = message.from_wire(decoded_data)
+    return dns_packet.question
+
+def get_dns_packet_answers(base64_data):
+    decoded_data = base64.b64decode(base64_data)
+    dns_packet = message.from_wire(decoded_data)
+    return dns_packet.answer
+
+def get_dns_packet_raw(base64_data):
     decoded_data = base64.b64decode(base64_data)
     dns_packet = message.from_wire(decoded_data)
     return dns_packet
 
-# 示例经过 Base64 转换的 Answer 字段值
-base64_answer = "mZaBgAABAAEAAAAAAmNuBGJpbmcDY29tAAABAAHADAABAAEAAAAKAATKWelk"
+# 创建命令行参数解析器
+parser = argparse.ArgumentParser(description='Extract DNS packet fields')
+parser.add_argument('-a', '--answer', required=True, help='Base64 encoded DNS answer')
+args = parser.parse_args()
 
-# 解码并解析 DNS 数据包
-dns_packet = decode_and_parse_dns_packet(base64_answer)
+# 提取 DNS 数据包的字段值
+dns_packet_id = get_dns_packet_id(args.answer)
+dns_packet_questions = get_dns_packet_questions(args.answer)
+dns_packet_answers = get_dns_packet_answers(args.answer)
+dns_packet_raw = get_dns_packet_raw(args.answer)
 
-# 访问 DNS 数据包字段示例
-print("DNS Packet ID:", dns_packet.id)
-print("Questions:", dns_packet.question)
-print("Answers:", dns_packet.answer)
-# 进一步访问和解析其他字段以获取更多信息
-
+# 打印提取的字段值
+print("DNS Packet ID:", dns_packet_id)
+print("Questions:", dns_packet_questions)
+print("Answers:", dns_packet_answers)
+print("Raw:", dns_packet_raw)
