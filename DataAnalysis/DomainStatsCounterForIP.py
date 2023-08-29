c848dee4fd7795eab06d2ff719c06d5f0a1ea3de
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-08-29 11:03:00 +0800

    0.5.5

--- DataAnalysis/DomainStatsCounterForIP.py
+++ DataAnalysis/DomainStatsCounterForIP.py
@@ -34,9 +34,5 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', type=str, required=True, help='Path to output CSV file')
     parser.add_argument('-s', '--chunksize', type=int, default=10000, help='Number of lines to process at a time')
     parser.add_argument("-c", "--ip", required=True, help="Specify the IP to filter.")
-
     args = parser.parse_args()
-
-    main(args.input, args.output, args.chunksize, args.ip)
-    parser.add_argument("-c", "--ip", required=True, help="Specify the IP to filter.")
     main(args.input, args.output, args.chunksize, args.ip)
