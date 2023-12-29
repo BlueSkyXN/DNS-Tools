865b04646ef80c94c9110aeae75a43c1732ab5eb
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-12-29 10:27:54 +0800

    20231229-1027

--- Backup_tools/Auto_7z_Backup_ADH_Qlog.py
+++ Backup_tools/Auto_7z_Backup_ADH_Qlog.py
@@ -4,7 +4,27 @@ import os
 import shutil
 import tempfile
 import configparser
-from Notify_to_Webhook import NotifytoWebhook
+import requests  # 从 Notify_to_Webhook.py 导入
+import json  # 从 Notify_to_Webhook.py 导入
+
+def NotifytoWebhook(webhook_url, message):
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "msg_type": "text",
+        "content": {
+            "text": message
+        }
+    }
+
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        print("Message sent successfully.")
+    else:
+        print("Failed to send message. Response code:", response.status_code)
 
 def create_backup():
     # 读取配置文件
